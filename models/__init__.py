b3fca105e5edfed50424d4e1be8f23468d7e51bd
yellow-circle <paydayzcool@gmail.com>
2023-05-09 13:19:04 +1000

    Modified init files

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
-from .AlexNet import *
+"""from .AlexNet import *
 from .VGG import *
 from .ResNet import *
 from .LeNet import *
 from .DenseNet import *
 from .GoogleNet import *
 from .WideResNet import *
+"""
